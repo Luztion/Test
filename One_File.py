83d604e28739bce8ed3705d7908cbd14dd18c112
Luztion <43429790+Luztion@users.noreply.github.com>
2018-09-23 22:36:34 +0800

    Add files via upload

---
+++ One_File.py
@@ -0,0 +1,18 @@
+import builtwith
+import whois
+#
+tech_used = builtwith.parse('https://fanyi.baidu.com/#en/zh/');
+print(tech_used);
+print(whois.whois('https://fanyi.baidu.com/#en/zh/'));
+
+'''
+for x in range(1, 5):
+    for y in range(1, 5):
+        for i in range(1, 5):
+            print(x, y, i);
+'''
+'''
+yuyi = '123';
+yuyi=input("Go：");
+print("You daan：",yuyi);
+'''
